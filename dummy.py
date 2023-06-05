25ac2248bae31faaa7f77a55b5ed1107ee2553c5
kaustubh-CS <kaustubh.rai@contentstack.com>
2023-06-05 19:14:05 +0530

    dummy data added

---
+++ dummy.py
@@ -0,0 +1,7 @@
+# dummy_data.py
+
+# This is a dummy file with some sensitive-looking data.
+
+# Dummy AWS credentials
+AWS_ACCESS_KEY_ID = 'AKIAIOSFODNN7EXAMPLE'
+AWS_SECRET_ACCESS_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
